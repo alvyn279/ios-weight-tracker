7732efaa0ad0ce5e4ed8b007ef31b39215abc89c
alvyn le <alvyn279@gmail.com>
2022-02-08 22:00:01 -0800

    temp for refresh control

--- WeightTracker/components/Home/HomeScreen.tsx
+++ WeightTracker/components/Home/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import {
   ActivityIndicator,
   KeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
+  RefreshControl,
   StyleSheet,
 } from 'react-native';
 import { useHeaderHeight } from '@react-navigation/elements';
@@ -15,6 +16,7 @@ import { HomeScreenProps } from '../../utils/navigation';
 import { useAppDispatch, useAppSelector } from '../../store';
 import {
   selectLatestWeight,
+  selectLatestWeightLoading,
   selectPreferredWeightUnit,
 } from '../../store/weights/selectors';
 import { fetchLatestWeight } from '../../store/weights';
@@ -42,6 +44,12 @@ const styles = StyleSheet.create({
   },
 });
 
+const MIN_REFRESH_DELAY = 3000;
+
+const wait = (timeout: number) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
 const HomeScreen = (_props: HomeScreenProps) => {
   // Accessing params:
   //_props.route.params?.fromHistory;
@@ -53,36 +61,64 @@ const HomeScreen = (_props: HomeScreenProps) => {
   const initialWeight = useAppSelector<number | undefined>(selectLatestWeight);
   const preferredUnit = useAppSelector<HealthUnit>(selectPreferredWeightUnit);
 
+  const [refreshing, setRefreshing] = React.useState(false);
+
   useScrollToTop(scrollViewRef);
 
   useEffect(() => {
     dispatch(fetchLatestWeight());
   }, [dispatch]);
 
+  const refreshLatestWeight = useCallback(async () => {
+    setRefreshing(true);
+    await wait(1000);
+    setRefreshing(false);
+    console.warn('lol');
+  }, [setRefreshing]);
+
+  const refreshControl = useMemo(
+    () => (
+      <RefreshControl
+        style={{
+          backgroundColor: 'red',
+        }}
+        refreshing={true}
+        // onRefresh={refreshLatestWeight}
+        // progressViewOffset={100}
+      />
+    ),
+    [],
+  );
+
   return (
-    <SafeAreaView style={[styles.appView]}>
-      <KeyboardAvoidingView
-        behavior={'padding'}
-        style={[styles.fullWidth, styles.fullFlex]}
-        keyboardVerticalOffset={headerHeight}>
-        <ScrollView
-          ref={scrollViewRef}
-          contentContainerStyle={[
-            styles.scrollViewContainer,
-            { paddingBottom: tabBarHeight },
-          ]}>
-          {initialWeight !== undefined ? (
-            <WeightEditor
-              initialWeight={initialWeight}
-              weightUnit={preferredUnit}
-              onSavePress={() => {}}
-            />
-          ) : (
-            <ActivityIndicator style={[styles.loader]} />
-          )}
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    // <SafeAreaView style={[styles.appView]}>
+    //   <KeyboardAvoidingView
+    //     behavior={'padding'}
+    //     style={[styles.fullWidth, styles.fullFlex]}
+    //     keyboardVerticalOffset={headerHeight}>
+    <ScrollView
+      ref={scrollViewRef}
+      refreshControl={refreshControl}
+      // contentContainerStyle={[
+      //   styles.scrollViewContainer,
+      //   {
+      //     paddingBottom: tabBarHeight,
+      //     height: 20000,
+      //   },
+      // ]}
+    >
+      {initialWeight !== undefined ? (
+        <WeightEditor
+          initialWeight={initialWeight}
+          weightUnit={preferredUnit}
+          onSavePress={() => {}}
+        />
+      ) : (
+        <ActivityIndicator style={[styles.loader]} />
+      )}
+    </ScrollView>
+    //   </KeyboardAvoidingView>
+    // </SafeAreaView>
   );
 };
 
