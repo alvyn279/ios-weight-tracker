3d0bbdd0c723bbdc5802461f6f8184ea9da32fb9
alvyn le <alvyn279@gmail.com>
2022-02-25 00:01:30 -0800

    feat: implement action strip for date picker modal

--- WeightTracker/components/DateTimePicker/DateTimePicker.tsx
+++ WeightTracker/components/DateTimePicker/DateTimePicker.tsx
@@ -1,17 +1,43 @@
 import React, { useCallback, useState } from 'react';
+import {
+  Button,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import DTP from '@react-native-community/datetimepicker';
-import { Modal, StyleSheet, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import WTButton from '../WTButton';
 import Text from '../ThemedText';
 import { useTheme } from '../../hooks';
+import { i18n } from '../../utils/i18n';
 
 const styles = StyleSheet.create({
   container: {},
   modalContent: {
+    paddingHorizontal: 20,
+  },
+  modalContainer: {
     flexDirection: 'column',
     flex: 1,
-    paddingHorizontal: 20,
+  },
+  touchableDatePickerIcon: {
+    marginBottom: 20,
+  },
+  actionStrip: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 55,
+  },
+  actionStripButtonContainer: {
+    paddingHorizontal: 10,
+  },
+  modalTitle: {
+    fontSize: 18,
   },
 });
 
@@ -24,7 +50,7 @@ interface DateTimePickerProps {
  */
 const DateTimePicker: React.FC<DateTimePickerProps> = _props => {
   const [showPicker, setShowPicker] = useState<boolean>(false);
-  const { backgroundStyle } = useTheme();
+  const { backgroundStyle, navBarStyle, textStyle } = useTheme();
 
   const memoShowPicker = useCallback(
     (pickerShowing: boolean) => () => setShowPicker(pickerShowing),
@@ -35,15 +61,42 @@ const DateTimePicker: React.FC<DateTimePickerProps> = _props => {
   // TODO: add date time picker in modal content
   return (
     <View style={styles.container}>
-      <WTButton onPress={memoShowPicker(true)}>Open modal</WTButton>
+      <TouchableOpacity
+        onPress={memoShowPicker(true)}
+        style={[styles.touchableDatePickerIcon]}>
+        <Ionicons name={'alarm-outline'} size={40} color={textStyle.color} />
+      </TouchableOpacity>
       <Modal
         visible={showPicker}
         presentationStyle="pageSheet"
         animationType="slide"
         onRequestClose={memoShowPicker(false)}>
-        <View style={[backgroundStyle, styles.modalContent]}>
-          <Text>Allo</Text>
-          <WTButton onPress={memoShowPicker(false)}>Done</WTButton>
+        <View style={[backgroundStyle, styles.modalContainer]}>
+          <View style={[styles.actionStrip, navBarStyle]}>
+            <View style={[styles.actionStripButtonContainer]}>
+              <Button
+                title={i18n.general_close}
+                onPress={memoShowPicker(false)}
+              />
+            </View>
+            <View style={[styles.actionStripButtonContainer]}>
+              <Text style={[styles.modalTitle]}>
+                {i18n.weightEditor_dateTimePicker_title}
+              </Text>
+            </View>
+            <View style={[styles.actionStripButtonContainer]}>
+              <Button
+                title={i18n.general_done}
+                onPress={() => {
+                  // call weight setter
+                  memoShowPicker(false)();
+                }}
+              />
+            </View>
+          </View>
+          <View style={[styles.modalContent]}>
+            <WTButton onPress={memoShowPicker(false)}>Done</WTButton>
+          </View>
         </View>
       </Modal>
     </View>
--- WeightTracker/utils/i18n.ts
+++ WeightTracker/utils/i18n.ts
@@ -1,11 +1,14 @@
 export const i18n = {
-  general_somethingWentWrong: 'Something went wrong.',
   general_areYouSure: 'Are you sure?',
+  general_close: 'Close',
+  general_done: 'Done',
   general_save: 'Save',
+  general_somethingWentWrong: 'Something went wrong.',
   weightEditor_enterWeight_prompt: 'Enter your weight:',
   weightEditor_enterWeight_save: 'Save to Health',
   weightEditor_enterWeight_confirm_message: (weight: number) =>
     `You are about to save a weight of ${weight}lbs to Apple Health.`,
+  weightEditor_dateTimePicker_title: 'Select date and time',
   warningPanel_missingHealthPermissions:
     'Please allow all operations to your Apple Health App in Settings > Health > Data Access & Devices > WeightTracker',
 };
